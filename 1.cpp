6f025fdd60c34cbdc7fa90f72b0f642351702860
unknown <r.kargar@ec.iut.ac.ir>
2023-12-09 18:59:58 +0330

    errors was fixed

--- 1.cpp
+++ 1.cpp
@@ -1,40 +1,6 @@
-#include <iostream>
-using namespace std;
-
-class container {
-
-	int size;
-public:
-	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
-
-};
-
-class vector :public container {
-
-	int call_num;
-public:
-	explicit vector(int l) :len(l),size(1 * 100){
-		p = new float();
-	}
-	int len;
-	int& getlen() const {
-		call_num ++;
-		return len;
-	}
-	~vector() = default;
-};
-
-int main() {
-
-	container c1(100);
-	vector v1 = c1;
-	container& r1 = v1;
-	container c2 = 100;
-	c2.getsize() = 20;
-	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
-	cout << v2.getlen();
-}
+﻿Build started...
+1>------ Build started: Project: test, Configuration: Debug x64 ------
+1>test.cpp
+1>test.vcxproj -> D:\Codes\c++\kargah\kargah 8\test\x64\Debug\test.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Build started at 6:58 PM and took 04.929 seconds ==========
